efe29a41f4d007da8bbc5578b85cebd75787b583
Quentin-Pettitt <159151875+Quentin-Pettitt@users.noreply.github.com>
2024-03-08 13:39:49 +0000

    hello world

--- program.py
+++ program.py
@@ -1,5 +1,5 @@
 # My Python Program
 # Task:  Use the function myFunction to output a simple "Hello World!" statement
 
-def myFunction():
-  # My Function
+def myFunction(): 
+  return ("Hello World!")
